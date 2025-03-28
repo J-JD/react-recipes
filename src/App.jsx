41d8f59f85348540bd35c65919eae248a9cb91cc
J-JD <jamesjohndoe@protonmail.com>
2025-03-28 17:22:24 +0100

    feat: Add RecipTitle component
    It displays dynamic data inside the component, using the syntax
    "{}", sometimes called handlebars. These handlebars effectively
    switch to JS mode and run almost any valid JS.

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,12 @@
 import React from 'react';
+import RecipeTitle from './RecipeTitle.jsx';
+
 
 function App() {
     return(
         <article>
             <h1>Recipe Manager</h1>
+            <RecipeTitle />
         </article>
     )
 }
